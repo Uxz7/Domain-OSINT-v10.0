1a2bbfbf6cb2d3fd39a1e6d9fd4b0b1b71661d05
RussianHarvey <251854751+Uxz7@users.noreply.github.com>
2026-06-15 09:59:39 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
  * ============================================================
- *  Copyright (c) 2025 RussianHarvey
+ *  Copyright (c) 2026 RussianHarvey
  *  All rights reserved.
  *
  *  DOMAIN OSINT v10.0 - Defensive Security Edition
